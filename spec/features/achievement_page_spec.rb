7059cdb47ccf3ddc599fcab89da251f84795fda6
Johannes C. Meyer <jmeyer1@gci.com>
2019-01-07 09:55:13 -0900

    Add redcarpet gem and data factories

---
+++ spec/features/achievement_page_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+feature 'achievement page' do
+    scenario 'achievement public page' do
+        achievement = FactoryBot.create(:achievement, title: 'Completed it')
+        visit("/achievements/#{achievement.id}")
+        
+        expect(page).to have_content('Completed it')
+    end
+
+    scenario 'render markdown description' do
+        achievement = FactoryBot.create(:achievement, description: 'That *was* hard')
+        visit("/achievements/#{achievement.id}")
+        
+        expect(page).to have_content('<i>was</i>')
+    end
+end
